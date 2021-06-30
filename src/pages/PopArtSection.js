5d7bf8dde0817080695e05029dd3a3ee01cc7292
RafaelRCamargo <rafaelrakochinski@gmail.com>
2021-06-30 12:01:15 -0300

    update

--- src/pages/PopArtSection.js
+++ src/pages/PopArtSection.js
@@ -16,21 +16,25 @@ let PopArtSection = () => {
       
       <TextContainer name="pop-art" title="o que é?">
         A Pop art é um movimento artístico que surgiu nos anos 50 como forma de reação ao expressionismo abstrato e representava críticas à sociedade consumista, que vinha crescendo na época, além de trazer temas como sexualidade, fama e industrialização.
+        <br />
         Marcado pela utilização de cores vibrantes, componentes ostensivos da cultura popular e objetos industrializados, a pop art traz esses elementos como ferramenta de crítica, hoje podemos encontrar esse tipo de arte em grafismo, quadrinhos, design e na moda.
       </TextContainer>
       
       <TextContainer name="pop-art r" title="o que é?">
         A Pop art é um movimento artístico que surgiu nos anos 50 como forma de reação ao expressionismo abstrato e representava críticas à sociedade consumista, que vinha crescendo na época, além de trazer temas como sexualidade, fama e industrialização.
+        <br />
         Marcado pela utilização de cores vibrantes, componentes ostensivos da cultura popular e objetos industrializados, a pop art traz esses elementos como ferramenta de crítica, hoje podemos encontrar esse tipo de arte em grafismo, quadrinhos, design e na moda.
       </TextContainer>
       
       <TextContainer name="pop-art" title="o que é?">
         A Pop art é um movimento artístico que surgiu nos anos 50 como forma de reação ao expressionismo abstrato e representava críticas à sociedade consumista, que vinha crescendo na época, além de trazer temas como sexualidade, fama e industrialização.
+        <br />
         Marcado pela utilização de cores vibrantes, componentes ostensivos da cultura popular e objetos industrializados, a pop art traz esses elementos como ferramenta de crítica, hoje podemos encontrar esse tipo de arte em grafismo, quadrinhos, design e na moda.
       </TextContainer>
       
       <TextContainer name="pop-art r" title="o que é?">
         A Pop art é um movimento artístico que surgiu nos anos 50 como forma de reação ao expressionismo abstrato e representava críticas à sociedade consumista, que vinha crescendo na época, além de trazer temas como sexualidade, fama e industrialização.
+        <br />
         Marcado pela utilização de cores vibrantes, componentes ostensivos da cultura popular e objetos industrializados, a pop art traz esses elementos como ferramenta de crítica, hoje podemos encontrar esse tipo de arte em grafismo, quadrinhos, design e na moda.
       </TextContainer>
       
