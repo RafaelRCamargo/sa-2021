932877a5e7bbafd20a20c05d3c1863e022d90a56
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-07-02 00:07:21 -0300

    arte urbana quase pronta

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -42,16 +42,16 @@ const CarouselSlider = (props) => {
     return (
       <Carousel autoPlay interval={4000} infiniteLoop centerMode dynamicHeight emulateTouch showThumbs={false} showStatus={false}>
         <div>
-          <img alt="" src="https://bg1.com.br/wp-content/uploads/2015/12/JR-CT-007-Cl%C3%A1udio-Tozzi-P%C3%A1ssaro-ASTcPM-40-X-40-cm-frente.jpg" />
+          <img alt="" src="https://casaclaudia.abril.com.br/wp-content/uploads/2017/08/foto-kobra.jpg" />
         </div>
         <div>
-          <img alt="" src="https://i.pinimg.com/originals/b3/b2/35/b3b2352b05b4bf64e3ba0fe61310d271.jpg" />
+          <img alt="" src="https://eduardokobra.com/uploads/img/slide/19/thumb-800-0/44691fd75a7fba30cf3d2b4641d7aef9.jpg" />
         </div>
         <div>
-          <img alt="" src="https://www.leilaodearte.com/obras/claudio-tozzi-floresorganica-acrilica-sobre-tela-13529.jpg" />
+          <img alt="" src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2957161:1592532303/foto.jpg?f=default&$p$f=aa10fe5" />
         </div>
         <div>
-          <img alt="" src="https://bg1.com.br/wp-content/uploads/2015/06/CT-003-Cl%C3%A1udio-Tozzi-Cidade-AST-110-X-145-cm-2000-frente.jpg" />
+          <img alt="" src="https://viagemeturismo.abril.com.br/wp-content/uploads/2019/03/imigrantes-kobra.jpg?quality=70&strip=info&w=680&h=453&crop=1" />
         </div>
       </Carousel>
     )
@@ -60,16 +60,16 @@ const CarouselSlider = (props) => {
     return (
       <Carousel autoPlay interval={4000} infiniteLoop centerMode dynamicHeight emulateTouch showThumbs={false} showStatus={false}>
         <div>
-          <img alt="" src="https://bg1.com.br/wp-content/uploads/2015/12/JR-CT-007-Cl%C3%A1udio-Tozzi-P%C3%A1ssaro-ASTcPM-40-X-40-cm-frente.jpg" />
+          <img alt="" src="https://i.pinimg.com/originals/12/57/82/125782c5452e0f23b497234e2e4f1ce7.jpg" />
         </div>
         <div>
-          <img alt="" src="https://i.pinimg.com/originals/b3/b2/35/b3b2352b05b4bf64e3ba0fe61310d271.jpg" />
+          <img alt="" src="https://pbs.twimg.com/media/DOFBCDTXkAAr81_.jpg" />
         </div>
         <div>
-          <img alt="" src="https://www.leilaodearte.com/obras/claudio-tozzi-floresorganica-acrilica-sobre-tela-13529.jpg" />
+          <img alt="" src="https://irp-cdn.multiscreensite.com/89dc4eb3/dms3rep/multi/Foto_007-2020+%28Rodrigo+Rizo%29.jpg" />
         </div>
         <div>
-          <img alt="" src="https://bg1.com.br/wp-content/uploads/2015/06/CT-003-Cl%C3%A1udio-Tozzi-Cidade-AST-110-X-145-cm-2000-frente.jpg" />
+          <img alt="" src="https://imagemdailha.com.br/fmanager/imgilha2016/noticias/imagem2797_1.png" />
         </div>
       </Carousel>
     )
