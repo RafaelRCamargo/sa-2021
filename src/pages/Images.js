d1a586087aba55b099c1250817ce3d87c6efbb0a
Rafael Rakochinski Camargo <66796237+RafaelRCamargo@users.noreply.github.com>
2021-07-19 11:23:30 -0300

    Update Images.js

--- src/pages/Images.js
+++ src/pages/Images.js
@@ -1,21 +1,6 @@
 import React from 'react'
-import api from "../services/api";
 
 const Images = () => {
-    /*const [data, setData] = useState()
-
-    useEffect(() => {
-        async function loadImages() {
-            await api
-                .get("imagem/")
-                .then((response) => {
-                    setData(response)
-                    console.log(data);
-                });
-            }
-            loadImages()
-        }, [])*/
-    
     return (
             <div className="container-Images">
                 <div className="container-body-images">
