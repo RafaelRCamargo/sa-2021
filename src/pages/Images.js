371aed95545ef5a7da982a1c4508872e2cb50f3a
Rafael Rakochinski Camargo <66796237+RafaelRCamargo@users.noreply.github.com>
2021-08-13 08:33:49 -0300

    Update Images.js

--- src/pages/Images.js
+++ src/pages/Images.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import api from "../services/api";
 import "./Images.css"
 const Images = () => {
-    const [url, setUrl] = useState([])
+    const [url] = useState([])
     const [req, setReq] = useState()
 
     useEffect(() => {
